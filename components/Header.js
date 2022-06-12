ab9d13a64893321ef71cb7e59757ced495ad223f
jakobpotosme <56290282+jakobpotosme@users.noreply.github.com>
2022-06-11 21:08:18 -0700

    First commit

---
+++ components/Header.js
@@ -0,0 +1,32 @@
+import Link from 'next/link'
+import React from 'react'
+// import resume from '../public/files/resume.pdf'
+
+function Header() {
+  return (
+    <div className='mx-auto flex justify-between px-24'>
+        
+        <h1>Jakob Potosme</h1>
+
+        <div className='flex flex-row gap-x-10 justify-center items-center '>
+            <ul className='flex flex-row gap-x-10  '>
+                <li>
+                    <Link href="/" >Blog</Link>
+                </li>
+                <li>
+                    <Link href="/" >Web3</Link>
+                </li>
+            </ul>
+            
+
+            <div className=' p-2 border-blue-400 border-2 rounded-md '>
+                <Link href="public\resume.pdf" >Resume</Link>
+            </div>
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default Header
