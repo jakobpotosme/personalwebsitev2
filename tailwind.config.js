34cc3216b23b13dee391d1263eb290489679c57f
jakobpotosme <56290282+jakobpotosme@users.noreply.github.com>
2022-06-29 10:10:40 -0700

    Extended min px

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   ],
   theme: {
     screens: {
-        'xs':{'min': '425px', 'max':'575px'},
+        'xs':{'min': '375px', 'max':'575px'},
        'sm': {'min': '576px', 'max': '767px'},
         'md': {'min': '768px', 'max': '991px'},
         'lg': {'min': '992px', 'max': '1199px'},
