e4408c0db63059f4369091a258825ed07c925459
jakobpotosme <56290282+jakobpotosme@users.noreply.github.com>
2022-07-03 00:00:19 -0700

    Removing unfinished work

--- pages/blog.js
+++ pages/blog.js
@@ -17,7 +17,7 @@ function Blog() {
   
   return (
      <>
-     {/* <div className='mx-auto flex justify-between px-24 pt-12'>
+     <div className='mx-auto flex justify-between px-24 pt-12'>
         
         <Link href='/' className='text-xl'>
             <TiArrowLeftOutline size={40} className=' hover:fill-[#00A789]'></TiArrowLeftOutline>
@@ -27,27 +27,8 @@ function Blog() {
         
 
     </div>
-    <div className='flex justify-center items-center h-screen'>Coming soon...</div> */}
-    <Header />
-    <div className='flex justify-center pt-20'>
-
-        <h1 className='font-semibold text-lg'>Welcome to my blog. This is where I will be writing about things I have learned in my programming journey.</h1>
-    </div>
-
-    <div>
-
-      {articles && articles.map(elem =>{
-        return(
-          <Articles key={elem.id} title={elem.title} description={elem.description} 
-          cover_img={elem.cover_image} url={elem.canonical_url} published_at={elem.published_at}
-          comments_count={elem.comments_count} page_views_count={elem.page_views_count}
-          reading_time_minutes={elem.reading_time_minutes}
-          
-          />
-        )
-      })}
-      
-    </div>
+    <div className='flex justify-center items-center h-screen'>Coming soon...</div>
+    
     </>
   )
 }
